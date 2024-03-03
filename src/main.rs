13d87e4d6523226eb7c3ab83230c56f5920cc1c6
Anatom <49984698+anatom3000@users.noreply.github.com>
2024-03-03 16:05:27 +0100

    add interactive prompt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
-use std::fs;
+use std::{fs, error::Error, io::Write};
 
 mod lexer;
 mod parser;
 mod tree;
 mod typer;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let Some(source_file_path) = std::env::args().nth(1)
-        else { return Err("a source file was expected but no argument was provided".into()) };
+fn process_file(path: String) -> Result<typer::Environment, Box<dyn Error>> {
+    let source = fs::read_to_string(path)?;
+    let mut env = typer::Environment::new();
 
-    let source = fs::read_to_string(source_file_path)?;
+    process(&source, &mut env);
+
+    println!("---");
+
+    Ok(env)
+}
+
+fn process(source: &str, env: &mut typer::Environment) {
+    
     let tokens = lexer::Lexer::new(&source).lex();
 
     assert!(tokens.errors.is_empty());
 
     let decls = parser::Parser::new(tokens.tokens).parse();
     
-    let mut env = typer::Environment::new();
     for d in decls {
         env.declaration(d);
     }
 
-    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut env = match std::env::args().nth(1) {
+        Some(source_file_path) => process_file(source_file_path)?,
+        None => typer::Environment::new(),
+    };
+    
+    let stdin = std::io::stdin();
+    let mut input = String::new();
+    loop {
+        input.clear();
+        print!("> ");
+        std::io::stdout().flush()?;
+
+        while &input[(input.len() as isize - 2).max(0) as usize..] != ";\n" {
+            stdin.read_line(&mut input)?;
+        }
+
+        input.pop();
+        input.pop();
+        
+        process(&input, &mut env);
+    }
 }
